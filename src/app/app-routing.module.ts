ab7fc4b15df3cd594c0317ebb6ec29cd0527bec7
Rajneesh Joshi <rajneeshjoshi4@gmail.com>
2019-08-05 17:37:18 +0530

    test
    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { PostsComponent } from './posts/posts.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.scss']
+})
+export class PostsComponent implements OnInit {
+  @ViewChild('form', { static: true }) inputPostForm: NgForm;
+  posts: any;
+  dataUrl: 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  getPost() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe(response => {
+        this.posts = response;
+        console.log(this.posts)
+      })
+  }
+
+  createPost(title, body) {
+    let post: any = { title: title, body: body };
+    this.http.post('https://jsonplaceholder.typicode.com/posts', post)
+      .subscribe(response => {
+        post.id = response.id;
+        this.posts.splice(this.posts.length, 0, post);
+        console.log(response);
+      })
+  }
+
+  onSubmit() {
+    //console.log(this.inputPostForm.value.title, this.inputPostForm.value.body);
+
+    this.inputPostForm.reset();
+  }
+
+  ngOnInit() {
+    this.getPost();
+  }
+
+}
